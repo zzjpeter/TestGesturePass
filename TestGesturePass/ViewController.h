b828392e0c40af391e58427da4878be80c607e79
zhuzhijia <zhuzhijia@lespark.us>
2019-05-09 09:45:51 +0800

    Initial Commit

---
+++ TestGesturePass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGesturePass
+//
+//  Created by 朱志佳 on 2019/5/9.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGesturePass/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestGesturePass
+//
+//  Created by 朱志佳 on 2019/5/9.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
